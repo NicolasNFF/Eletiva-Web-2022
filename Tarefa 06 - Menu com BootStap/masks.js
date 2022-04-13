cd81f6119ae648d4c6c2a9393d0031d2ddb97c6f
NicolasNFF <79235449+NicolasNFF@users.noreply.github.com>
2022-04-12 23:11:05 -0300

    Create masks.js

---
+++ Tarefa 06 - Menu com BootStap/masks.js
@@ -0,0 +1,9 @@
+$('#inputPrice').mask("#.##0,00", {reverse: true});
+
+
+function getAsNumber(inputId){
+    var price = document.getElementById(inputId).value;
+    price = price.replace(/\./g, '').replace(',', '.');
+    
+    return price;
+}
